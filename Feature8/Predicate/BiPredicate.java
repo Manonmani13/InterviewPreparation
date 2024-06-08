55a3f7b910c595187ca871869d8bc268a6461a8b
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-08 17:45:23 +0530

    Bi Predicate

--- Feature8/Predicate/BiPredicate.java
+++ Feature8/Predicate/BiPredicate.java
@@ -1,5 +1,6 @@
 package com.predicate;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 public class BiPredicate {
@@ -9,6 +10,16 @@ public static void main(String[] args) {
 	java.util.function.BiPredicate<Integer,Integer> bp=(a,b)->a>=50 && b<50;
 	boolean b=bp.test(60, 30);
 	System.out.println(b);
+	List<Person> pl=PredicatePersonExample.getAllPersons();
+
+	java.util.function.BiPredicate<Integer,String> ls=(salary,name)->salary>1000 && name.contains("ano");
+	pl.forEach(per->{
+		if(ls.test(per.getSalary(), per.getName()))
+			System.out.println(per);
+
+	}
+					
+			);
 	
 }
 }
