7c4e0653d05e02db22d9a4a611b7b21e55d274f1
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-10 21:38:35 +0530

    method reference

--- Feature8/methodReference/MethodReferenceExample.java
+++ Feature8/methodReference/MethodReferenceExample.java
@@ -7,7 +7,9 @@ public class MethodReferenceExample {
     public static void main(String[] args) {
         System.out.println(MethodReferenceExample.compare(10,20));
         Comparator<Integer> comp=MethodReferenceExample::compare;
+        Comparator<Integer> comp2=(c,b)->compare(c,b);
         System.out.println("In Java 8"+comp.compare(20,20));
+        System.out.println(comp2.compare(20,30));
     }
     public static int compare(Integer a,Integer b)
     {
