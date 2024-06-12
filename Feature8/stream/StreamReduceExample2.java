7aeef580229adef70ce45ff4f96f9ac2a3a5fa11
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-12 21:50:09 +0530

    reduce

--- Feature8/stream/StreamReduceExample2.java
+++ Feature8/stream/StreamReduceExample2.java
@@ -3,9 +3,12 @@ package com.stream;
 import com.function.FunctionWithPerson;
 import com.function.Person;
 
+import java.util.Optional;
+
 public class StreamReduceExample2 {
     public static void main(String[] args) {
         combineName();
+        getSalary();
     }
     static void combineName()
     {
@@ -14,5 +17,13 @@ public class StreamReduceExample2 {
                 .reduce("",(a,b)->a.concat(b));
         System.out.println(names);
     }
+    static void  getSalary()
+    {
+        Optional<Person> salary=FunctionWithPerson.getAllPersonList().stream()
+                .reduce((x,y)->x.getSalary()>y.getSalary()?x:y);
+        System.out.println(salary);
+    }
+
 
 }
+
