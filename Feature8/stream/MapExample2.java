8688960b8c506a55106675519e96a972ea2bb5cc
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-12 07:56:01 +0530

    map example

--- Feature8/stream/MapExample2.java
+++ Feature8/stream/MapExample2.java
@@ -4,12 +4,16 @@ import com.function.FunctionWithPerson;
 import com.function.Person;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class MapExample2 {
+    static Function<Person,String> fun= per->per.getName();
+    static Function<String,String> upper=pers->pers.toLowerCase();
     public static void main(String[] args) {
         List<Person> personList= FunctionWithPerson.getAllPersonList();
         System.out.println(getPersonNames(personList));
+        personList.stream().map(fun).map(upper).collect(Collectors.toList()).forEach(System.out::println);
     }
     static List<String> getPersonNames(List<Person> person)
     {
@@ -17,3 +21,4 @@ public class MapExample2 {
     }
 
 }
+
