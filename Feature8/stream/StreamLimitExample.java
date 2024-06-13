60ee841c9822d918e6cb83988c0ad41d98b62637
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-13 07:45:41 +0530

    limit example

--- Feature8/stream/StreamLimitExample.java
+++ Feature8/stream/StreamLimitExample.java
@@ -7,6 +7,10 @@ public class StreamLimitExample {
     public static void main(String[] args) {
         List<String> furits= Arrays.asList("Apple","Orange","Mango","PineApple");
         furits.stream().limit(3).forEach(System.out::println);
+        List<Integer> list=Arrays.asList(1,2,3,4,5,6,7,8,9);
+        int sum=list.stream().limit(3).reduce(0,(x,y)->x+y);
+        System.out.println(sum);
 
     }
 }
+
