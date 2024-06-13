1f841da1b9dbb19989800464fce979632cdcdabb
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-13 21:15:24 +0530

    long and double stream

---
+++ Feature8/numbericStream/NumbericStreamFactoryExample.java
@@ -0,0 +1,18 @@
+package com.numbericStream;
+
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class NumbericStreamFactoryExample {
+    public static void main(String[] args) {
+        IntStream l1=IntStream.range(1,6);
+        IntStream l2=IntStream.rangeClosed(1,6);
+        l1.forEach(System.out::println);
+        System.out.println("====================");
+        l2.forEach(System.out::println);
+        LongStream.rangeClosed(1,50).forEach(System.out::println);
+        DoubleStream ds=LongStream.rangeClosed(1,20).asDoubleStream();
+        ds.forEach(System.out::println);
+    }
+}
