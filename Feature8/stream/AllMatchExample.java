1dfd131ab416aac01ef30ce6cba65a0d7723489e
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-13 07:37:16 +0530

    all match and any match example

--- Feature8/stream/AllMatchExample.java
+++ Feature8/stream/AllMatchExample.java
@@ -1,5 +1,8 @@
 package com.stream;
 
+import com.function.FunctionWithPerson;
+import com.function.Person;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
@@ -12,6 +15,14 @@ public class AllMatchExample {
         System.out.println(furits.stream().allMatch(p));
         System.out.println(furits.stream().allMatch(p2));
         System.out.println(furits.stream().anyMatch(p2));
+        Predicate<Person> personPredicate=per->per.getSalary()>2000;
+        boolean allmatch= FunctionWithPerson.getAllPersonList().stream()
+                .allMatch(personPredicate);
+        System.out.println("Person match"+allmatch);
+        boolean anymatch= FunctionWithPerson.getAllPersonList().stream()
+                .anyMatch(personPredicate);
+        System.out.println("Person match"+anymatch);
 
     }
 }
+
