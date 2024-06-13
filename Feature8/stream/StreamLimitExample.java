4fcc975524dbd7d03b66d0c89726efa7bca91370
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-13 07:47:06 +0530

    limit example

--- Feature8/stream/StreamLimitExample.java
+++ Feature8/stream/StreamLimitExample.java
@@ -10,6 +10,8 @@ public class StreamLimitExample {
         List<Integer> list=Arrays.asList(1,2,3,4,5,6,7,8,9);
         int sum=list.stream().limit(3).reduce(0,(x,y)->x+y);
         System.out.println(sum);
+        int skip=list.stream().skip(3).reduce(0,(x,y)->x+y);
+        System.out.println(skip);
 
     }
 }
