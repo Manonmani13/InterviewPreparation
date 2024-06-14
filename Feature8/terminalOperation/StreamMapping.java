1f7af78026fdc816018d53fd6d34a1ac14c5c7f5
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-14 06:12:23 +0530

    terminal operation

---
+++ Feature8/terminalOperation/StreamMapping.java
@@ -0,0 +1,28 @@
+package com.terminalOperation;
+
+import com.function.FunctionWithPerson;
+import com.function.Person;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamMapping {
+    static List<String> getNames()
+    {
+        List<String> ls= FunctionWithPerson.getAllPersonList().stream()
+                .map(Person::getName)
+                .collect(Collectors.toList());
+                return ls;
+    }
+    static List<String> usingTerminalOperation()
+    {
+        List<String> ls= FunctionWithPerson.getAllPersonList().stream().collect(Collectors.mapping( Person::getName,Collectors.toList()));
+        return ls;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getNames());
+        System.out.println(usingTerminalOperation());
+    }
+
+}
