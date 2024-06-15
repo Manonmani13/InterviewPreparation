26e2bba643031f437a43b41fc7b45eb76e201934
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-15 10:26:23 +0530

    optional

---
+++ Feature8/Optional/OptionalExample.java
@@ -0,0 +1,31 @@
+package com.Optional;
+
+import com.function.Person;
+
+import java.util.Optional;
+
+public class OptionalExample {
+    static String myName(String name)
+    {
+        return name;
+    }
+    static String personName(Person per)
+    {
+        if(null!=per)
+        {
+            return per.getName();
+        }
+        else{
+            return "No Record Found";
+        }
+    }
+
+    public static void main(String[] args) {
+        Optional<String> name=Optional.ofNullable(myName("Naveeen"));
+        System.out.println(name.get());
+        Optional<String> name1=Optional.ofNullable(null);
+//        System.out.println(name1.get());
+        System.out.println(name1.isPresent()?name1.get():"No data");
+    }
+
+}
