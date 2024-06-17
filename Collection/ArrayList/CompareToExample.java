9dca1e9960eac627b273e73e960d187a6b1976b5
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-17 07:22:55 +0530

    compareto example

---
+++ Collection/ArrayList/CompareToExample.java
@@ -0,0 +1,11 @@
+package com.collection.ArrayList;
+
+public class CompareToExample {
+    public static void main(String[] args) {
+        String name="vishal";
+        String name2="Suriyan";
+        String name3="nithya";
+        int result=name.compareTo(name2);
+        System.out.println(result);
+    }
+}
