14780ed00283998a8ecc5499c1a26b29e6864847
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-17 06:56:21 +0530

    arryaist exmple

---
+++ Collection/ArrayList/Laptop.java
@@ -0,0 +1,14 @@
+package com.collection.ArrayList;
+
+public class Laptop {
+    int price;
+
+    int gb;
+    boolean touch;
+
+    public Laptop(int price, int gb, boolean touch) {
+        this.price = price;
+        this.gb = gb;
+        this.touch = touch;
+    }
+}
