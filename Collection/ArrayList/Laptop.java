89743f60c067f1e5f1224dc23dad8c24d1245cd9
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-17 07:31:32 +0530

    comparato

--- Collection/ArrayList/Laptop.java
+++ Collection/ArrayList/Laptop.java
@@ -5,10 +5,29 @@ public class Laptop {
 
     int gb;
     boolean touch;
+    String moodel;
 
     public Laptop(int price, int gb, boolean touch) {
         this.price = price;
         this.gb = gb;
         this.touch = touch;
     }
+
+    public Laptop(int price, int gb, boolean touch, String moodel) {
+        this.price = price;
+        this.gb = gb;
+        this.touch = touch;
+        this.moodel = moodel;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "price=" + price +
+                ", gb=" + gb +
+                ", touch=" + touch +
+                ", moodel='" + moodel + '\'' +
+                '}';
+    }
 }
+
