3737d8918374ea727fc349088c24106c63c2546c
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-17 22:28:19 +0530

    hash eampel

---
+++ Collection/Hash/TreeSetExample.java
@@ -0,0 +1,16 @@
+package com.collection.Hash;
+
+import java.util.TreeSet;
+
+public class TreeSetExample {
+    public static void main(String[] args) {
+        TreeSet ts=new TreeSet();
+        ts.add(12);
+        ts.add(2);
+        ts.add(3);
+//        ts.add("naveen");
+        //it is not support
+        System.out.println(ts);
+
+    }
+}
