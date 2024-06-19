5684d9d4f7db89468f69f710ea22e96bb3d2fced
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-19 22:34:19 +0530

    hashmap

--- Collection/Hash/HashMapEample.java
+++ Collection/Hash/HashMapEample.java
@@ -3,6 +3,9 @@ package com.collection.Hash;
 import com.collection.ArrayList.Laptop;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class HashMapEample {
     public static void main(String[] args) {
@@ -14,8 +17,22 @@ public class HashMapEample {
         hm.put(lenovo,124);
         hm.put(hp   ,125);
         System.out.println(hm);
+        Set s=hm.entrySet();
+        Iterator i=s.iterator();
+        while (i.hasNext())
+        {
+            Map.Entry me= (Map.Entry) i.next();
+            Object k=me.getKey();
+            Object o=me.getValue();
+            Laptop lap= (Laptop) k;
+            if(o.equals(124)){
+                System.out.println("price "+ lap.price);
 
+            }
+
+        }
 
 
     }
 }
+
