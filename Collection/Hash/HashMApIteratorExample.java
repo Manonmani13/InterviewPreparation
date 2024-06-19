4332d0143b0bc7fdcb6da7411e6bc0a5f9e9ed8d
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-19 21:10:41 +0530

    hashmap

---
+++ Collection/Hash/HashMApIteratorExample.java
@@ -0,0 +1,28 @@
+package com.collection.Hash;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMApIteratorExample {
+    public static void main(String[] args) {
+        HashMap hm=new HashMap();
+        hm.put("Nellai Exp",1900);
+        hm.put("Mannai Exp",2000);
+        hm.put("Mumbai Exp",2100);
+        hm.put("Vangi Exp",2100);
+        Set s=hm.entrySet();
+        System.out.println(s);
+        Iterator i=s.iterator();
+        while(i.hasNext())
+        {
+            Object o=i.next();
+            Map.Entry e= (Map.Entry) o;
+
+            System.out.println(e.getKey()+" : "+e.getValue());
+        }
+
+
+    }
+}
