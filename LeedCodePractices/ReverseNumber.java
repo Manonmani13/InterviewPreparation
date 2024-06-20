fb85dc78a2841a4988b96f09e83f140e51b9920a
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-20 21:56:17 +0530

    reverse integer

--- LeedCodePractices/ReverseNumber.java
+++ LeedCodePractices/ReverseNumber.java
@@ -2,23 +2,49 @@ package com.leedcode;
 
 public class ReverseIntegers {
     public static void main(String[] args) {
-        int x=-123,d=0;
+        int x=1534236469,d=0;
         int  no=0;
-        while(x!=0)
-        {
-            d=x%10;
-            if (no > Integer.MAX_VALUE / 10 || (no == Integer.MAX_VALUE / 10 && d > 7)) {
-                no=no+0;
+//        while(x!=0)
+//        {
+//            d=x%10;
+//            if (no > Integer.MAX_VALUE / 10 || (no == Integer.MAX_VALUE / 10 && d > 7)) {
+//                no=no+0;
+//            }
+//            if (no < Integer.MIN_VALUE / 10 || (no == Integer.MIN_VALUE / 10 && d < -8)) {
+//                no=no+0;
+//
+//            }
+//            no=no*10+(d);
+//
+//            x=x/10;
+//        }
+//        System.out.println(no);
+//        System.out.println(x);
+        System.out.println(reverse(x));
+    }
+    public static int reverse(int x) {
+        int reversed = 0;
+        System.out.println(x);
+        while (x != 0) {
+            int digit = x % 10;
+            System.out.println("digit "+digit);
+            // Check for overflow before multiplying by 10
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                System.out.println(reversed +" "+Integer.MAX_VALUE);
+                return 0; // Return 0 if overflow would occur
             }
-            if (no < Integer.MIN_VALUE / 10 || (no == Integer.MIN_VALUE / 10 && d < -8)) {
-                no=no+0;
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+                System.out.println(reversed +" "+Integer.MIN_VALUE);
 
+                return 0; // Return 0 if overflow would occur
             }
-            no=no*10+(d);
-
-            x=x/10;
+            System.out.println(reversed * 10 );
+            System.out.println(reversed * 10 +digit);
+            reversed = reversed * 10 + digit;
+            System.out.println(reversed );
+            x = x / 10;
         }
-        System.out.println(no);
+        return reversed;
     }
 }
 
