93886b5e684bb6c43984b8bdf10c3453ab4fa5a5
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-22 21:37:03 +0530

    linked list

---
+++ Collection/LinkedList/LinkedListExampleJTP.java
@@ -0,0 +1,14 @@
+package com.collection.LinkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListExampleJTP {
+    public static void main(String[] args) {
+        LinkedList<String> l=new LinkedList<>();
+        l.add("Mano");
+        l.add("Naveen");
+        l.add("Pandukumar");
+        l.add("MP");
+        System.out.println(l);
+    }
+}
