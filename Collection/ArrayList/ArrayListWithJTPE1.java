6b510884278271bfb58bfe5905a61622dca7afd0
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-22 21:31:15 +0530

    arraylist

---
+++ Collection/ArrayList/ArrayListWithJTPE1.java
@@ -0,0 +1,14 @@
+package com.collection.ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayListWithJTPE1 {
+    public static void main(String[] args) {
+        ArrayList<String> list=new ArrayList<>();
+        list.add("mano");
+        list.add("pandukumar");
+        list.add("naveen");
+        list.add("Selvam");
+        System.out.println(list);
+    }
+}
