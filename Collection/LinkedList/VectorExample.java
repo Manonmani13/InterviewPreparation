03f288813b30d3b14df9047233effdab65cbf8fd
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-22 21:44:38 +0530

    vector

---
+++ Collection/LinkedList/VectorExample.java
@@ -0,0 +1,13 @@
+package com.collection.LinkedList;
+
+import java.util.Vector;
+
+public class VectorExample {
+    public static void main(String[] args) {
+        Vector<String> v=new Vector<>();
+        v.add("Mano");
+        v.add("Naveen");
+        v.add("Pandu");
+        System.out.println(v);
+    }
+}
