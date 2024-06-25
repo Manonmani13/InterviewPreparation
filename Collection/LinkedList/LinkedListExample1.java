4cc0340deee094d2d18f788ef4c9284dba9d6243
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-25 21:09:08 +0530

    linked list

---
+++ Collection/LinkedList/LinkedListExample1.java
@@ -0,0 +1,13 @@
+package com.collection.LinkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListExample1 {
+    public static void main(String[] args) {
+        LinkedList<String> ll=new LinkedList<>();
+        ll.add("Mano");
+        ll.add("Manonmani");
+        ll.add("Naveen");
+        System.out.println(ll);
+    }
+}
