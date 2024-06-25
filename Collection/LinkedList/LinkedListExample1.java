7d0980517cb8454ac78ca1a6cd3997b7b378c610
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-25 21:12:07 +0530

    linked list

--- Collection/LinkedList/LinkedListExample1.java
+++ Collection/LinkedList/LinkedListExample1.java
@@ -9,5 +9,12 @@ public class LinkedListExample1 {
         ll.add("Manonmani");
         ll.add("Naveen");
         System.out.println(ll);
+        LinkedList<String> ll1=new LinkedList<>();
+        ll1.addAll(ll);
+        ll1.add("Pandukumar");
+        ll1.add("Kumar");
+        ll1.addFirst("Chandar");
+        ll1.addLast("Maha");
+        System.out.println(ll1);
     }
 }
