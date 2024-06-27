fc9244d993f041a7444a5f82a9e41e6f4e876498
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-27 20:42:56 +0530

    linkedlist

---
+++ Collection/LinkedList/LinkedHashSetExample.java
@@ -0,0 +1,13 @@
+package com.collection.LinkedList;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetExample {
+    public static void main(String[] args) {
+        LinkedHashSet<String> lhs=new LinkedHashSet<>();
+        lhs.add("Manonmani");
+        lhs.add("Pandukumar");
+        lhs.add("Naveen");
+        System.out.println(lhs);
+    }
+}
