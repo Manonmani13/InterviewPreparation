3163ee966f7ffe857ebe044021b312321478f435
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-27 21:11:28 +0530

    filter

---
+++ Feature8/filter/FilterExample.java
@@ -0,0 +1,28 @@
+package com.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilterExample {
+    public static void main(String[] args) {
+        List<Product> productsList = new ArrayList<Product>();
+        productsList.add(new Product(1,"HP Laptop",25000f));
+        productsList.add(new Product(2,"Dell Laptop",30000f));
+        productsList.add(new Product(3,"Lenevo Laptop",28000f));
+        productsList.add(new Product(4,"Sony Laptop",28000f));
+        productsList.add(new Product(5,"Apple Laptop",90000f));
+        productsList.stream().filter(p->p.price>20000).map(p->p.price).forEach(System.out::println);
+    }
+}
+class Product{
+    int id;
+    String name;
+    float price;
+    public Product(int id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
+
+
