bf0fd133d2cb35551ee96375811333e8f2f058d7
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-06-29 22:34:43 +0530

    colelction

---
+++ Collection/statck/TreeSetExample.java
@@ -0,0 +1,13 @@
+package com.proretention.scrm.calendar;
+
+import java.util.TreeSet;
+
+public class TreeSetExample {
+    public static void main(String[] args) {
+        TreeSet ts=new TreeSet();
+        ts.add("Manonmani");
+        ts.add("Pandukumar");
+        ts.add("Maha");
+        System.out.println(ts);
+    }
+}
