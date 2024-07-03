a13517e047cc1bb705800e75ed02c780f66d6ac7
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-07-03 21:49:46 +0530

    thread

---
+++ ThreadExample/ExecExample.java
@@ -0,0 +1,10 @@
+package com.proretention.scrm.admin;
+
+import java.io.IOException;
+
+public class ExecExample {
+    public static void main(String[] args) throws IOException {
+        Runtime.getRuntime().exec("notepad");
+
+    }
+}
