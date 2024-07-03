175c9259a764e0f8d7e39a559925d6dc2a68722c
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-07-03 21:50:35 +0530

    thread

---
+++ ThreadExample/ThreadSycnRExample.java
@@ -0,0 +1,51 @@
+package com.proretention.scrm.calendar;
+
+public class ThreadSycnRExample {
+    public static void main(String[] args) throws Exception {
+        Table t=new Table();
+        Thread1 r=new Thread1(t);
+        Thread2 r2=new Thread2(t);
+        r.start();
+        r2.start();
+   }
+}
+class Table{
+    synchronized public void printTables(int n)
+    {
+        for(int i=1;i<=5;i++)
+        {
+            try{
+                Thread.sleep(400);
+            }catch(Exception e){System.out.println(e);}
+
+            System.out.println(n*i);
+        }
+    }
+
+}
+class Thread1 extends Thread
+{
+    Table t;
+    Thread1(Table t)
+    {
+        this.t=t;
+    }
+
+    @Override
+    public void run() {
+        t.printTables(3);
+    }
+}
+class Thread2 extends Thread
+{
+    Table t;
+    Thread2(Table t)
+    {
+        this.t=t;
+    }
+
+    @Override
+    public void run() {
+        t.printTables(5);
+    }
+}
