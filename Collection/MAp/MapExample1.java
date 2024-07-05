a90549325d3da10b14066682d11585a12bf0b872
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-07-05 10:44:53 +0530

    map

---
+++ Collection/MAp/MapExample1.java
@@ -0,0 +1,14 @@
+package com.practices.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample1 {
+    public static void main(String[] args) {
+        Map map=new HashMap();
+        map.put(1,"Mano");
+        map.put(2,"Naveen");
+        map.put(3,"Pandu");
+        System.out.println(map);
+    }
+}
