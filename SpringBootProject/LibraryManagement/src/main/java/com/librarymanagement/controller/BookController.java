7013340ff4957fe569d04536c918c11bc436aa85
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-07-06 20:03:52 +0530

    library management

--- SpringBootProject/LibraryManagement/src/main/java/com/librarymanagement/controller/BookController.java
+++ SpringBootProject/LibraryManagement/src/main/java/com/librarymanagement/controller/BookController.java
@@ -3,6 +3,7 @@ package com.librarymanagement.controller;
 import com.librarymanagement.entity.Book;
 import com.librarymanagement.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,5 +31,28 @@ public class BookController {
     {
         return bookService.save(book);
     }
+    @DeleteMapping("/{id}")
+    public void saveBook(@PathVariable Long id)
+    {
+         bookService.deleteById(id);
+    }
+    @PostMapping("/{bookId}/borrow/{userID}")
+    public ResponseEntity<Book> borrowBook(@PathVariable Long bookId,@PathVariable Long userId)
+    {
+        Book borrow=bookService.borrowBook(bookId,userId);
+        if(borrow!=null)
+            return ResponseEntity.ok(borrow);
+        else
+            return ResponseEntity.badRequest().build();
+    }
 
+    @PostMapping("/{bookId}/return")
+    public ResponseEntity<Book> returnBook(@PathVariable Long bookId)
+    {
+        Book returnbook= bookService.returnBook(bookId);
+        if(returnbook!=null)
+         return    ResponseEntity.ok(returnbook);
+        else
+         return    ResponseEntity.badRequest().build();
+    }
 }
