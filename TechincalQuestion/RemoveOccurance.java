85e826b1aa9806988cfd6ca3fe7eec86c24d7f60
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-07-14 14:54:18 +0530

    RemoveOccurance.java

---
+++ TechincalQuestion/RemoveOccurance.java
@@ -0,0 +1,8 @@
+package com.practices.techinicalQuestion;
+
+public class RemoveOccurance {
+    public static void main(String[] args) {
+        String s="Manonmani Pandukumar";
+        System.out.println(s.replace("a",""));
+    }
+}
