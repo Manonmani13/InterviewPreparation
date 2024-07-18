b495d57c4945a3d558880ea3fc4e616cb6299bac
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-07-18 07:47:12 +0530

    oneto many

--- SpringBootProject/OneToOneSampleExample/src/main/java/com/simple/onetoonesample/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.simple.onetoonesample.repository;
-
-import com.simple.onetoonesample.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Customer,Long> {
-}
