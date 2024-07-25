cf9cd661db9758de587dc5af513cc2d47e847768
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-07-25 22:39:45 +0530

    Pattern

---
+++ DesignPattern/mediator/MediatorPatternDemo.java
@@ -0,0 +1,8 @@
+package com.proretention.scrm.Integration.mediator;
+
+public class MediatorPatternDemo {
+    public static void main(String[] args) {
+        User robert=new User("Mano");
+        robert.sendMessage("Hi Mano");
+    }
+}
