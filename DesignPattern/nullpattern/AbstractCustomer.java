a9cbb86575cb81cea5e3d38b604e1643dad60544
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-07-26 21:34:34 +0530

    Pattern

---
+++ DesignPattern/nullpattern/AbstractCustomer.java
@@ -0,0 +1,7 @@
+package com.proretention.scrm.Integration.nullpattern;
+
+public abstract class AbstractCustomer {
+    protected String name;
+    public abstract boolean isNil();
+    public abstract String getName();
+}
