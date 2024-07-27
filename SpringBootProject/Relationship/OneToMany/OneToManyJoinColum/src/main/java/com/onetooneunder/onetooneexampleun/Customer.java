e667699dda6438bc0f99cf52649652c4b7c78c0b
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-07-27 20:47:50 +0530

    one to many relationship

---
+++ SpringBootProject/Relationship/OneToMany/OneToManyJoinColum/src/main/java/com/onetooneunder/onetooneexampleun/Customer.java
@@ -0,0 +1,52 @@
+package com.onetooneunder.onetooneexampleun;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name="customer_id")
+    private List<Item> item=new ArrayList<>();
+
+    public List<Item> getItem() {
+        return item;
+    }
+
+    public void setItem(List<Item> item) {
+        this.item = item;
+    }
+
+    public Customer() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Customer(String name) {
+        this.name = name;
+    }
+}
---
+++ SpringBootProject/Relationship/OneToMany/OneToManyJoinColum/src/main/java/com/onetooneunder/onetooneexampleun/Item.java
@@ -0,0 +1,44 @@
+package com.onetooneunder.onetooneexampleun;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue
+
+    private Long id;
+
+    private String name;
+
+
+
+    public Item() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+//    @OneToOne(cascade = CascadeType.ALL,mappedBy = "item")
+//
+//    private Customer customer;
+
+}
