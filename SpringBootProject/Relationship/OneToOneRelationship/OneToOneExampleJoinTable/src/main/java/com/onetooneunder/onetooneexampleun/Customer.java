246e0e3055e636bba01cc533bb515db1e068e7b8
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-07-27 20:16:09 +0530

    one to one relationship

---
+++ SpringBootProject/Relationship/OneToOneRelationship/OneToOneExampleJoinTable/src/main/java/com/onetooneunder/onetooneexampleun/Customer.java
@@ -0,0 +1,54 @@
+package com.onetooneunder.onetooneexampleun;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    //if you dont want item_id as name of cloumn in customer table then you can use join clounm annotation
+//    @JoinColumn(name="fk_item")
+// if you want to create new table with two ids of the class you can use below method
+    @JoinTable(name="custom_item",joinColumns = {
+            @JoinColumn(name="customer_id")},
+            inverseJoinColumns = {@JoinColumn(name="item_id")})
+    private Item item;
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public Customer() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Customer(String name) {
+        this.name = name;
+    }
+}
