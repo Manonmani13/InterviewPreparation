5034b3e7a76df75906b724ce2162f32133369055
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-08-05 15:31:45 +0530

    Capital

---
+++ InterviewAskedQuestion/FirstLetterCapitial.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class FirstLetterCapitial {
+	public static void main(String[] args) {
+		String input="manonmani";
+		System.out.println(input.substring(0,1).toUpperCase()+input.substring(1).toLowerCase());
+		
+	}
+
+}
