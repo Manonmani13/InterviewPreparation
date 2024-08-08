987dc6e20bc81b6e39bf74fd0753a2d9aa44a63f
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-08-08 15:09:59 +0530

    exception

---
+++ PracticesPrograms/ExceptionQuestion.java
@@ -0,0 +1,16 @@
+package com.test;
+
+
+public class Test {
+	public static void main(String[] args) {
+		try{
+			float a = 10/0;
+			}catch(Exception e){
+			System.out.println("exception");
+			}
+//		catch(ArithmeticException ae){
+//				System.out.println("exception");
+//			}
+	}
+
+}
