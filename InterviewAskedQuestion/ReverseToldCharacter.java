6ecfc9566d2d09b471c22d2eb543c32937604dc0
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-08-09 15:20:38 +0530

    Reverse to string

--- InterviewAskedQuestion/ReverseToldCharacter.java
+++ InterviewAskedQuestion/ReverseToldCharacter.java
@@ -4,15 +4,25 @@ public class ReverseToldCharacter {
 	public static void main(String[] args) {
 		String name="abcdefg";
 		int k=2;
-		String sublit=name.substring(0,k);
-		String newString="";
-		for(int i=sublit.length()-1;i>=0;i--)
+//		String sublit=name.substring(0,k);
+//		String newString="";
+//		for(int i=sublit.length()-1;i>=0;i--)
+//		{
+//			newString+=sublit.charAt(i);
+//		}
+//		newString+=name.substring(k);
+//		System.out.println(newString);
+		StringBuffer stb=new StringBuffer();
+		for(int i=0;i<name.length();i+=2*k)
 		{
-			newString+=sublit.charAt(i);
+			StringBuilder sb=new StringBuilder();
+			int end=Math.min(i+k, name.length());
+			sb.append(name.substring(i, end)).reverse();
+			stb.append(sb);
+			int nextEnd=Math.min(i+2*k, name.length());
+			stb.append(name.substring(end,nextEnd));
 		}
-		newString+=name.substring(k);
-		System.out.println(newString);
-		
+		System.out.println(stb);
 	}
 
 }
