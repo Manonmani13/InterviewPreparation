72da662611d48d92e41cc88407ed4c68cd200efa
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-08-10 20:41:45 +0530

    Roman to int

---
+++ LeedCodePractices/RomanToInt.java
@@ -0,0 +1,20 @@
+package com.test;
+
+public class RomanToInt {
+public static void main(String[] args) {
+	String s="III";
+	 int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+     String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+     int result=0;
+     for(int i=0;i<symbols.length;i++)
+     {
+    	 while(s.startsWith(symbols[i]))
+    	 {
+    		 result+=values[i];
+    		 s=s.substring(symbols[i].length());
+    	 }
+     }
+     System.out.println(result);
+}
+}
+
