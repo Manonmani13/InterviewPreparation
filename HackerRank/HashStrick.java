2db488225b423cab9c075494bb87e4e131ce48f6
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-08-14 21:26:12 +0530

    hashstick

---
+++ HackerRank/HashStrick.java
@@ -0,0 +1,20 @@
+package com.hackerrank;
+
+public class HashStrick {
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<=n;i++)
+	    {
+	        for(int j=i;j<n;j++)
+	        {
+	            System.out.print(" ");
+	        }
+	        for(int k=1;k<=i;k++)
+	        {
+	            System.out.print("#");
+	        }
+	        System.out.println();
+	    }
+	}
+
+}
