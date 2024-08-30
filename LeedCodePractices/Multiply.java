4221d65523ebad05c8452e726409689fdba3b990
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-08-30 21:19:26 +0530

    big int

---
+++ LeedCodePractices/Multiply.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+import java.math.BigInteger;
+
+public class Multiply {
+	public static void main(String[] args) {
+		BigInteger big=new BigInteger("2");
+		BigInteger big2=new BigInteger("3");
+		BigInteger result=big.multiply(big2);
+		System.out.println(result);
+	}
+
+}
