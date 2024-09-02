9c67ea5442aaa9882714f0d8a944b08156b0e4dd
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-09-02 21:56:01 +0530

    find samples functions

--- AShokITSpringBootLearning/1stApllicationAshokIt/src/main/java/com/ashokit/Application.java
+++ AShokITSpringBootLearning/1stApllicationAshokIt/src/main/java/com/ashokit/Application.java
@@ -1,5 +1,8 @@
 package com.ashokit;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -20,9 +23,25 @@ public class Application {
 		emp.setEmpName("Mano");
 		emp.setEmpId(1);
 		employeeRepository.save(emp);
+		Employee e=new Employee();
+		e.setEmpAge(27);
+		e.setEmpName("Naveen");
+		e.setEmpId(2);
+		employeeRepository.save(e);
+
+		Employee e1=new Employee();
+		e1.setEmpAge(30);
+		e1.setEmpName("Chandar");
+		e1.setEmpId(3);
+		employeeRepository.save(e1);
 		Employee findUser=employeeRepository.findByEmpName("Mano");
 		System.out.println(findUser);
+		List<Employee> grater=employeeRepository.findByEmpAgeGreaterThan(25);
+		System.out.println(grater);
+
+		List<Employee> names=employeeRepository.findByEmpNameIn(Arrays.asList("Mano","Naveen"));
 		
+		System.out.println(names);
 	}
 
 }
--- AShokITSpringBootLearning/1stApllicationAshokIt/src/main/java/com/ashokit/repository/EmployeeRepository.java
+++ AShokITSpringBootLearning/1stApllicationAshokIt/src/main/java/com/ashokit/repository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.ashokit.repository;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,8 @@ import com.ashokit.entity.Employee;
 @Repository
 public interface EmployeeRepository  extends CrudRepository<Employee, Serializable>{
 	public Employee findByEmpName(String name);
+		public List<Employee> findByEmpAgeGreaterThan(Integer age);
+		public List<Employee> findByEmpNameIn(List<String> name);
+
 
 }
