eeb1b2a9cb5b5389a489a1c25a6273b35a78907c
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-09-04 15:00:16 +0530

    2nd program

--- AShokITSpringBootLearning/2stApllicationAshokIt-1/src/main/java/com/ashokit/EmployeeService.java
+++ AShokITSpringBootLearning/2stApllicationAshokIt-1/src/main/java/com/ashokit/EmployeeService.java
@@ -2,32 +2,37 @@ package com.ashokit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EmployeeService {
-	@Autowired
-	private EmployeeRepository empRepository;
-	@Autowired
-	private AddressRepository addRepository;
-	
-//	@Transactional(rollbackOn = Exception.class)
-	public void saveData()
-	{
-		Employee emp=new Employee();
-		emp.setEmpId(5);
-		emp.setEmpName("Pandukumar");
-		emp.setEmpAge(55);
-		empRepository.save(emp);
-		int i=10/0;
-		Address add=new Address();
-		add.setAddId(1);
-		add.setCity("Chennai");
-		add.setCountry("India");
-		add.setState("Tamil Nadu");
-		add.setEmpId(5);
-		addRepository.save(add);
-	}
+    @Autowired
+    private EmployeeRepository empRepository;
+    
+    @Autowired
+    private AddressRepository addRepository;
+    
+    @Transactional(rollbackFor = Exception.class)
+    public void saveData() {
+        Employee emp = new Employee();
+        emp.setEmpId(6);
+        emp.setEmpName("Pandukumar E");
+        emp.setEmpAge(55);
+        empRepository.save(emp);
+        System.out.println("after saving employee");
 
+        // This line will cause an ArithmeticException, triggering a rollback
+        int i = 10 / 0;
+
+        // This code will not be executed due to the exception above
+        System.out.println("not coming");
+        
+        Address add = new Address();
+        add.setAddId(1);
+        add.setCity("Chennai");
+        add.setCountry("India");
+        add.setState("Tamil Nadu");
+        add.setEmpId(6);
+        addRepository.save(add);
+    }
 }
