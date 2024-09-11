ec340f9ff839550d9d4f4af105efa660dee1ac2a
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-09-11 21:41:25 +0530

    angular

--- Angular/todo-frontend/src/app/task/task.component.ts
+++ Angular/todo-frontend/src/app/task/task.component.ts
@@ -3,26 +3,23 @@ import { Component } from '@angular/core';
 import { TaskService } from './task.service';
 import { Task } from './task.model';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms'
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,TaskComponent],
+  imports: [CommonModule,RouterOutlet,TaskComponent,FormsModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
+  newTask: Task = { description: "", completed: false };
 
-  constructor(private taskService:TaskService)
-  {
-    newTask:Task={description:"",completed:false};
-    createTask():void{
-      this.taskService.createTask(this.taskService).subscribe((createTask)=>
-    {
-      this.newTask={description:"",completed:false};
+  constructor(private taskService: TaskService) {}
 
-    })
-
-    }
+  createTask(): void {
+    this.taskService.createTask(this.newTask).subscribe((createdTask: Task) => {
+      this.newTask = { description: "", completed: false };
+    });
   }
 }
