d2fbf7c0c5b12368db1a31cf44efa9d6c8f740eb
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-09-15 18:21:04 +0530

    function

--- JavaScript/Introduction/script.js
+++ JavaScript/Introduction/script.js
@@ -1,27 +1,29 @@
-console.log("Hello")
-        alert("Hey ")
-        let score=0
-        score=10
-        console.log("Hello your score is " +score)
+// console.log("Hello")
+//         alert("Hey ")
+//         let score=0
+//         score=10
+//         console.log("Hello your score is " +score)
 
-        //excersie
-        let amount=1000
-        console.log("you have to pay amount "+amount)
+//         //excersie
+//         let amount=1000
+//         console.log("you have to pay amount "+amount)
 
-        const name="Mano"
-        console.log("NAme "+name);
-        //name="test"  cannot change
-        no='5'
-        console.log(typeof no)
-        console.log(no==5)//value same or not only check
-        console.log(no===5) //value and data type
-        console.log(no!==5)//not equal check
-        n=100
-        console.log(n>50&& n%10==0)
-        console.log("or "+n>50||n%10==0)
+//         // const name="Mano"
+//         console.log("NAme "+name);
+//         //name="test"  cannot change
+//         no='5'
+//         console.log(typeof no)
+//         console.log(no==5)//value same or not only check
+//         console.log(no===5) //value and data type
+//         console.log(no!==5)//not equal check
+//         n=100
+//         console.log(n>50&& n%10==0)
+//         console.log("or "+n>50||n%10==0)
 
-        console.log("single "+n>50 & n%10==0)
+//         console.log("single "+n>50 & n%10==0)
 
-        // console(no-1)
-        let ask=prompt("what is your name");
-        console.log("hi "+ask)
+//         // console(no-1)
+//         let ask=prompt("what is your name");
+//         console.log("hi "+ask)
+//         let a=prompt("enter a number")
+//         console.log(Number(a)+10)
