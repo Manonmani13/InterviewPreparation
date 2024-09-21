9552663759130ef5187114e69806e703c7c12358
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-09-21 19:01:36 +0530

    function defion

---
+++ JavaScript/Introduction/functionsSample/script.js
@@ -0,0 +1,19 @@
+function isPostive(num)
+{
+    return num>0
+}
+console.log(isPostive(-2))
+function sayHello()
+{
+    console.log('hello')
+}
+function findProduct(a,b)
+{
+    return a*b;
+}
+console.log(findProduct(4,7))
+function greet(name='there')
+{
+    console.log('hi',name)
+}
+greet('mano')
