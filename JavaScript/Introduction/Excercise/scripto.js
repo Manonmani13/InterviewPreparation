f6ce35b4d97caf86b857dcaf236cbdf252641959
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-09-21 13:04:52 +0530

    excer

--- JavaScript/Introduction/Excercise/scripto.js
+++ JavaScript/Introduction/Excercise/scripto.js
@@ -1,4 +1,7 @@
 const button = document.querySelector('button')
+let resultdiv = document.createElement('div')
+resultdiv.id = 'result'
+document.getElementById('wrapper').appendChild(resultdiv)
 button.addEventListener('click',displayStats)
 function displayStats(){
     console.log("hello")
@@ -6,7 +9,33 @@ function displayStats(){
     const city=input.options[input.selectedIndex].value
     console.log(city)
     let population = 0, literacyRate = 0, language =''
+    switch(city)
+    {
+        case 'Bengaluru':
+            population = 8443675
+            literacyRate = 88.71
+            language = 'Kannada'
+            break
+            case 'Chennai':
+                population = 4646732
+                literacyRate = 90.20
+                language = 'Tamil'
+                break
+            case 'Mumbai':
+                population = 12442373
+                literacyRate = 89.73
+                language = 'Marathi'
+                break
+            case 'Delhi':
+                population = 16787941
+                literacyRate = 86.20
+                language = 'Hindi'
+                break
+
+    }
     let text = `The Indian city of ${city} has a population of ${population}. Language spoken is ${language} and literacy rate is ${literacyRate}%.`
     console.log(text)
-    
+    document.getElementById('result').innerHTML = text
+
+
 }
