875f362672633701aaeedb36b203d2eac5c52a32
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-09-21 13:56:41 +0530

    cal

---
+++ JavaScript/Introduction/jobeligible/script.js
@@ -0,0 +1,22 @@
+let pwd=true
+if(pwd)
+    console.log("you are logged in")
+else
+console.log("incorrect password")
+age=58
+gender='female'
+if(age>60&&gender=='female')
+    console.log("You can avail special discount")
+let a=20,b=30;
+let max;
+
+if(a>b)
+    max=a;
+else
+max=b;
+max=a>b?a:b
+console.log(max)
+
+fd=10
+avg=(fd<3)?5.8:(fd>3&&fd<6)?6.5:(fd>7&&fd<9)?6.8:(fd>=10)?7:0;
+console.log(avg)
