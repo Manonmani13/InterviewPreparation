441b2b8c61e4e2fffa78f3e6062cf0694c21fc0f
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-09-22 09:19:41 +0530

    set and get

--- JavaScript/Introduction/classExample/script.js
+++ JavaScript/Introduction/classExample/script.js
@@ -51,3 +51,43 @@ let paidUser1=new PaidUser('Mano',23);
 paidUser1.message()
 //method chaining
 paidUser1.login().message()
+
+function User(name,age)
+{
+        this.name=name;
+        this.age=age;
+        // this.loginu=function()
+        // {
+        //     console.log("you are logged in")
+        // }
+
+}
+User.prototype.loginu=function()
+{
+    console.log('hi',this.name)
+    console.log("you are logged in")
+}
+let user1=new User('Mano',23)
+user1.loginu()
+
+//get and set
+class Temperature
+{
+    constructor( temp  )
+    {
+        this.temp=temp
+    }
+    get temp()
+    {
+        return this.temp
+    }
+    set temp(temp)
+    {
+        if(temp>100)
+            temp=100
+        this.temp=temp
+    }
+}
+let  temp1=new Temperature(25)
+temp1.temp=140
+console.log(temp1.temp)
