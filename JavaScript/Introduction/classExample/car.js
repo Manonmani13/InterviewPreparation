6ab37f33a447a400a85d68b5bdbae7de5bfb8bc6
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-09-22 09:44:13 +0530

    syn

--- JavaScript/Introduction/classExample/car.js
+++ JavaScript/Introduction/classExample/car.js
@@ -15,3 +15,20 @@ console.log('Reparing ')
 //this is one option
 // export default Car
 // export {fillGas,repair}
+
+console.log('hello')
+function Sync()
+{
+    console.log('step 1')
+    console.log('step 2')
+    console.log('step 3')
+
+}
+Sync()
+let a=100
+let b=20
+let c=a+b
+setTimeout(()=>console.log('step1'),4000)
+setTimeout(()=>console.log('step2'),2000)
+setTimeout(()=>console.log('step3'),3000)
+console.log('bye')
