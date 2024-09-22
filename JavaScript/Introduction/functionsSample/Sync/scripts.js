49be9e0f142315808b8c0d419506cf2d88ef69f0
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-09-22 10:44:10 +0530

    promise

--- JavaScript/Introduction/functionsSample/Sync/scripts.js
+++ JavaScript/Introduction/functionsSample/Sync/scripts.js
@@ -14,3 +14,14 @@ function failure()
 {
     console.log("Thanks for trying ! I will book a bus")
 }
+function tossCoint()
+{
+    return new Promise((reslove,reject)=>{
+        const rand=Math.floor(Math.random()*2)
+        if(rand==0)
+            resolve()
+        else
+        reject()
+    })
+}
+tossCoint().then.(()=>console.log("Congrations you won")).catch(()=>console.log("you loss"))
