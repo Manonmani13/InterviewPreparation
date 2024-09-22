aea6f4ec5ceceb3cc0d0140f6f0f718a35ed8204
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-09-22 10:53:27 +0530

    async

--- JavaScript/Introduction/exception/script.js
+++ JavaScript/Introduction/exception/script.js
@@ -14,3 +14,17 @@ catch(error){
 finally{
     console.log('bye')
 }
+async function fn()
+{
+    return 'helo'
+}
+console.log(fn())
+fn().then((msg)=>console.log(msg))
+async function asyncstatus() {
+    console.log('hi ..')
+    res=await reachA 
+    console.log(res)
+    console.log('bye')
+
+}
+asyncstatus()
