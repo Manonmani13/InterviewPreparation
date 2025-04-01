3578e90bb094e291696b5451d103ad06a65f905a
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2025-04-01 21:07:00 +0530

    Add files via upload

---
+++ array.ts
@@ -0,0 +1,11 @@
+let fruits:string[];
+fruits=['mango','apple','banana','orange'];
+console.log(fruits);
+
+let animal:Array<string>
+animal=['Triger','Lion','Rabbit'];
+console.log(animal);
+
+let persons:Array<string|number>;
+persons=['Ram',20,'Anil',30];
+console.log(persons);
